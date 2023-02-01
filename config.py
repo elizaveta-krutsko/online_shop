b2372a75caa41084a7651335d2871f0960aa607b
ElizavetaKrutko <deh9jke@gmail.com>
2023-02-01 20:59:56 +0300

    env settings and gitignore

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+dotenv_path = Path('/home/oem/PycharmProjects/online_shop/envs/.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 DB_DIALECT: str = os.getenv('DB_DIALECT')  #postgresql
 DB_HOST: str = os.getenv('DB_HOST')  #localhost
 DB_USERNAME: str = os.getenv('DB_USERNAME')  #online_shop
 DB_PASSWORD: str = os.getenv('DB_PASSWORD')  #root
 DB_NAME: str = os.getenv('DB_NAME')  #postgres
-DB_PORT: int = os.getenv('DB_PORT')  #5432
+DB_PORT: str = str(os.getenv('DB_PORT'))  #5432
--- sql_online_shop/database.py
+++ sql_online_shop/database.py
@@ -6,11 +6,11 @@ import config as conf
 #SQLALCHEMY_DATABASE_URL = "jdbc:postgresql://localhost:5432/postgres"
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:root@localhost:5432/online_shop'
 #SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = f'{conf.DB_DIALECT}://{conf.DB_USERNAME}:{conf.DB_PASSWORD}@:{conf.DB_HOST}:{conf.DB_PORT}/{conf.DB_NAME}'
+SQLALCHEMY_DATABASE_URL = f'{conf.DB_DIALECT}://{conf.DB_USERNAME}:{conf.DB_PASSWORD}@{conf.DB_HOST}:{conf.DB_PORT}/{conf.DB_NAME}'
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
